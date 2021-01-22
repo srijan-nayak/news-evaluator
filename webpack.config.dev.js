897d03472f2a0a6d05f53ea1f7b42a3f98bdce92
Srijan Nayak <srijannayak0@gmail.com>
2021-01-22 20:08:37 +0530

    feat: Initial commit with webpack dev configuration

---
+++ webpack.config.dev.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/client/index.js"),
+  devtool: "cheap-module-source-map",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/client/html/index.html"),
+      title: "News Tone Analyser",
+    }),
+  ],
+};
