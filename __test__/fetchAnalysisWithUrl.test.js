7f491b0ce94a04368c4f34faf1217ad01d892705
Srijan Nayak <srijannayak0@gmail.com>
2021-01-25 19:22:09 +0530

    test: Add test for fetchAnalysisWithUrl function

---
+++ __test__/fetchAnalysisWithUrl.test.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const { fetchAnalysisWithUrl } = require("../src/server/fetchAnalysis");
+
+const API_KEY = process.env.API_KEY;
+const encodedUrl = encodeURIComponent(
+  "https://www.ndtv.com/india-news/deaths-of-six-healthcare-workers-not-linked-to-covid-vaccination-health-ministry-2357187"
+);
+
+test("Fetch sentiment analysis with valid api key and URL input", async () => {
+  const { data, ok } = await fetchAnalysisWithUrl({
+    url: encodedUrl,
+    apiKey: API_KEY,
+  });
+
+  ok ? expect(data.status.code).toBeDefined() : expect(data).toBeNull();
+});
