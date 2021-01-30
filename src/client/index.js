7211a12e8df2ac7f451272abab19c7a2484dee2a
Srijan Nayak <srijannayak0@gmail.com>
2021-01-30 19:31:30 +0530

    feat: Add styled input form

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,4 @@
 import "./sass/header.scss";
 import "./sass/main.scss";
+import "./sass/inputs-section.scss";
 import "./js/app";
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,1 +1,6 @@
 console.log("stuff works");
+
+const inputsForm = document.querySelector(".inputs-section form");
+inputsForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
