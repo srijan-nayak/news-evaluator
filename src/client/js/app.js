c722eaf657285983dc13be8eb0d2651b6d350282
Srijan Nayak <srijannayak0@gmail.com>
2021-01-31 20:18:30 +0530

    feat: Validate active input

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,8 +1,16 @@
 import toggleDisabledInputs from "./toggleDisabledInput";
+import getActiveInput from "./getActiveInput";
+import hasValidValue from "./hasValidValue";
 
 toggleDisabledInputs();
 
 const inputsForm = document.querySelector(".inputs-section form");
 inputsForm.addEventListener("submit", (event) => {
+  const activeInput = getActiveInput();
+  if (hasValidValue(activeInput)) {
+    console.log("input is valid");
+  } else {
+    console.log("input is invalid");
+  }
   event.preventDefault();
 });
