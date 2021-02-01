5b24d1134ef553ffc90f0ab5ff08b7ec96b3560f
Srijan Nayak <srijannayak0@gmail.com>
2021-02-01 20:26:26 +0530

    chore: Setup initial webpack production mode config

---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/client/index.js"),
+  devtool: "source-map",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/client/html/index.html"),
+      title: "News Tone Analyser",
+    }),
+  ],
+};
