6ab9cfe1f3c3de1fc9f031d61725b97687853949
Srijan Nayak <srijannayak0@gmail.com>
2021-02-01 20:34:27 +0530

    chore: Extract css file while building for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "production",
@@ -19,7 +20,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -29,5 +30,6 @@ module.exports = {
       template: path.resolve(__dirname, "src/client/html/index.html"),
       title: "News Tone Analyser",
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
