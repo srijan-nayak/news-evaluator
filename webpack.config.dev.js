4a19b2f45d63440d4f4ebd11046060d3cde71969
Srijan Nayak <srijannayak0@gmail.com>
2021-02-03 20:59:33 +0530

    chore: Add workbox to webpack dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -29,5 +30,6 @@ module.exports = {
       template: path.resolve(__dirname, "src/client/html/index.html"),
       title: "News Tone Analyser",
     }),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
